a7b13fba07cc9d2f5a0a864085dd14a81fe2c270
TobiaszKowalczyk <252943@edu.p.lodz.pl>
2026-05-07 21:14:34 +0200

    wip

--- task-3/main.py
+++ task-3/main.py
@@ -1,5 +1,127 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras.applications import VGG16,EfficientNetV2M,ResNet50
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import(
+    confusion_matrix,
+    classification_report,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_curve,
+    auc
+)
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 16
+
+train_dir = 'dataset/train'
+test_dir = 'dataset/test'
+validation_dir = 'dataset/validation'
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical'
+)
+
+val_data = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical',
+    shuffle=False
+)
+
+
+
+class_names = list(train_data.class_indices.keys())
+
+def create_vgg16():
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
+
+    base_model.trainable = False
+
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(train_data.num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
+
+def create_resnet50():
+    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
+
+    base_model.trainable = False
+
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(train_data.num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+def create_efficientnetv2m():
+    base_model = EfficientNetV2M(weights='imagenet', include_top=False, input_shape=(224,224,3))
+
+    base_model.trainable = False
+
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(train_data.num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+
 def main():
-    print("Hello from task-3!")
+    print(class_names[2])
 
 
 if __name__ == "__main__":
